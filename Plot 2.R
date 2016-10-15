e1ce543c52ae96777c4b969ac838cefbbdb81191
niisaban <abdulhammond@hotmail.com>
2016-10-15 18:10:16 -0400

    Update Plot 2.R

--- Plot 2.R
+++ Plot 2.R
@@ -1,6 +1,14 @@
 ## The aim of this file is to provide a R code that is capable of constructing plot 2
 ## Obtain the full dataset by downloading the file into a data folder
 
+## download file into data folder
+dataset_url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(dataset_url, "Dataset.zip")
+
+## Unzip the file
+unzip("Dataset.zip", exdir = "Dataset")
+setwd("C:/Users/Abdulrahman/Documents/Coursera/Exploratory Data Analysis/Dataset")
+
 data <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?",  nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 
